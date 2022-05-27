e55b0a2c0e8b7c315879529f04d8990d814c82ca
Hadizat Oluwaseun <hadizathmotunrayo@gmail.com>
2022-05-27 17:12:28 +0100

    some changes

--- user_data.php
+++ user_data.php
@@ -9,20 +9,26 @@ if(isset ($_POST['submit'])){
       ['Gender: ', $_POST['gender']],  
       ['Country: ', $_POST['country']],  
     ];
-    
-    // echo "<pre>";
-// print_r($formData);
-    foreach($formData as $fD){
-        echo "<pre>";
-       print_r($fD);
-    } 
 
     $fp = fopen('userdata.csv', 'a');
 
-    foreach($formData as $key=>$data){;
+    foreach($formData as $key=>$data){
+
     fputcsv($fp, $data);
      }
     // fwrite($fp, $form);
 
     fclose($fp);
 }
+
+$file = fopen("userdata.csv", "r");
+  
+while (($data = fgetcsv($file)) !== false) {
+    echo "<pre>";
+    foreach ($data as $i) {
+        print_r ("<li>" . ($i).  "</li>" ) ."<br>";
+   
+    }
+    
+}
+fclose($file);
